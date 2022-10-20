8cadf0dac0e3e41c83eef81ca5ff1c2c03790655
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-10-20 18:37:11 -0500

    Fix test printers for safer tests

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -5,6 +5,8 @@
 #define VERSION "0.1"
 #include "sys/Cli.hpp"
 
+using namespace test;
+
 void segfault(int a) { API_ASSERT(false); }
 
 int main(int argc, char *argv[]) {
@@ -14,16 +16,14 @@ int main(int argc, char *argv[]) {
   signal(11, segfault);
 #endif
 
-  printer::Printer printer;
-
-  printer.set_verbose_level(cli.get_option("verbose"));
 
   {
     auto scope
-      = test::Test::Scope<printer::Printer>(test::Test::Initialize()
+      = Test::Scope<printer::Printer>(test::Test::Initialize()
                                               .set_name(cli.get_name())
                                               .set_version(VERSION)
                                               .set_git_hash(CMSDK_GIT_HASH));
+    Test::printer().set_verbose_level(cli.get_option("verbose"));
 
     UnitTest(cli.get_name()).execute(cli);
   }
